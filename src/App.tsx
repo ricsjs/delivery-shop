fb5779df02832be4f74ee5b35866bdffc65b9ec2
ricsjs <ricfilho0007@gmail.com>
2024-04-01 16:53:06 -0300

    config: delivery shop project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './global.css'
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routes"
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | delivery.shop'/>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
+}
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,5 @@
+import { Helmet } from "react-helmet-async"
+
+export function SignIn() {
+    return <><Helmet title="Login"/><h1>SignIn</h1></>
+}
